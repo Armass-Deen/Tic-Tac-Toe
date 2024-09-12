79908572047ca1791c8f550804e9b3c9787297eb
ArmassDeenD <armassdeveloper@gmail.com>
2024-09-12 18:29:41 +0530

    .

--- TIC- TAC-TOE/main.js
+++ TIC- TAC-TOE/main.js
@@ -4,7 +4,19 @@ function clickeing(id) {
     console.log(id);
     var div = document.getElementById(id);
     if(div.innerHTML== ""){
+        console.log(div);
+        
+        if(clicked%2 == 0){
+            div.innerHTML = 'Y' 
         h1.innerHTML="X TURN"
+        }
+        else{
+         div.innerHTML = 'X'
+          h1.innerHTML="Y TURN"
+
+        }
+
+       
      clicked++;
     }
     isWin();
@@ -19,48 +31,48 @@ const six=document.getElementById("6").innerHTML
 const seven=document.getElementById("7").innerHTML
 const eight=document.getElementById("8").innerHTML
 const nine=document.getElementById("9").innerHTML
-// const win=document.getElementById("win")
-// const hide=document.getElementById("hide")
+const win=document.getElementById("win")
+const hide=document.getElementById("hide")
 
-// if(one!= "" && one==two && two == three){
-//     win.innerHTML=`WINNER IS  ${one}`
-//     hide.style.display="block"
-// } 
-// else if (three!="" && three==four && four==five) {
-//    win.innerHTML=`WINNER IS  ${three}`
-//     hide.style.display="block"
-// }
-// else if(seven!="" && seven==eight && eight==nine){
-//      win.innerHTML=`WINNER IS  ${seven}`
-//       hide.style.display="block"
-// }
-// else if(one!="" && one==four && four==seven){
-//      win.innerHTML=`WINNER IS  ${one}`
-//       hide.style.display="block"
-// }
-// else if(two!="" && two==five && five==eight){
-//     win.innerHTML=`WINNER IS  ${two}`
-//      hide.style.display="block"
-// }
-// else if(three!="" && three==six && six==nine){
-//     win.innerHTML=`WINNER IS  ${three}`
-//      hide.style.display="block"
-// }
-// else if(one!="" && one==five && five==nine){
-//     win.innerHTML=`WINNER IS  ${one}`
-//      hide.style.display="block"
-// }
-// else if(seven!="" && seven==five && five==three){
-//     win.innerHTML=`WINNER IS  ${seven}`
-//      hide.style.display="block"
-// }
-// else if (four!=""&& four==five&& five==six){
-//     win.innerHTML=`WINNER IS  ${four}`
-// }
-// else if(one!="" && two!=""&& three!=""&& four!="" && five!="" && six!="" && seven!="" && eight!=""&& nine!=""){
-//     win.innerHTML="Ohh Itz DREAW !"
-//      hide.style.display="block"
-// }
+if(one!= "" && one==two && two == three){
+    win.innerHTML=`WINNER IS  ${one}`
+    hide.style.display="block"
+} 
+else if (three!="" && three==four && four==five) {
+   win.innerHTML=`WINNER IS  ${three}`
+    hide.style.display="block"
+}
+else if(seven!="" && seven==eight && eight==nine){
+     win.innerHTML=`WINNER IS  ${seven}`
+      hide.style.display="block"
+}
+else if(one!="" && one==four && four==seven){
+     win.innerHTML=`WINNER IS  ${one}`
+      hide.style.display="block"
+}
+else if(two!="" && two==five && five==eight){
+    win.innerHTML=`WINNER IS  ${two}`
+     hide.style.display="block"
+}
+else if(three!="" && three==six && six==nine){
+    win.innerHTML=`WINNER IS  ${three}`
+     hide.style.display="block"
+}
+else if(one!="" && one==five && five==nine){
+    win.innerHTML=`WINNER IS  ${one}`
+     hide.style.display="block"
+}
+else if(seven!="" && seven==five && five==three){
+    win.innerHTML=`WINNER IS  ${seven}`
+     hide.style.display="block"
+}
+else if (four!=""&& four==five&& five==six){
+    win.innerHTML=`WINNER IS  ${four}`
+}
+else if(one!="" && two!=""&& three!=""&& four!="" && five!="" && six!="" && seven!="" && eight!=""&& nine!=""){
+    win.innerHTML="Ohh Itz DREAW !"
+     hide.style.display="block"
+}
 }
 
 function resert()
